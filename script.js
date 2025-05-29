0d18a0c510ea0629397bbbe7f97c610b6c7ec96a
mangili-michele <mangili.michele.studente@itispaleocapa.it>
2025-05-29 13:48:39 +0000

    si

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let seconds = 0;
     
     // Emoji da utilizzare come simboli delle carte
-    const symbols = ['ciao', '🍌', '🍒', '🍓', '🍋', '🥝', '🍉', '🥭'];
+const symbols = ['ciao', '🍌', '🍒', '🍓', '🍋', '🥝', '🍉', '🥭','😀','😈','😍','😡'];
     
     // Inizializza il gioco
     function initGame() {
